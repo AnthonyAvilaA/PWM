efd9a1a2f9b340981d56f8886438c2336e89b7d7
Anthony Diego Avila Arias <anthony.avila101@alu.ulpgc.es>
2025-03-16 13:30:48 +0000

    News content injected

--- templates/home/index.js
+++ templates/home/index.js
@@ -8,66 +8,35 @@ fetch('/resources/json/news.json')
 
     len_data = news_data.length;
     
-    while (len_data > 0) {
-        for (news_article of top_stories_section.querySelectorAll('.article')) {
-            data = news_data[news_data.length-1];
-            
-            article_content = news_article.querySelector('.article-text-content');
-            article_content.querySelector('.title').innerHTML = `${data.Title}`;
-            article_content.querySelector('.extract').innerHTML = `${data.short_description}`;            
+    for (news_article of top_stories_section.querySelectorAll('.article')) {
+        data = news_data[news_data.length-1];
+        
+        article_content = news_article.querySelector('.article-text-content');
+        article_content.querySelector('.title').innerHTML = `${data.Title}`;
+        article_content.querySelector('.extract').innerHTML = `${data.short_description}`;            
+        article_content.querySelector('.read-more').href = `/templates/news/index.html?id=${data.documentId}`;            
+        
+        image_container = news_article.querySelector('.image-container img').src = `${data.image_url}`;
+        
+        news_data.pop();
+        len_data--;
+        if (len_data == 0) break;
+    }
 
-            image_container = news_article.querySelector('.image-container img').src = `${data.image_url}`;
-            
-            news_data.pop();
-            len_data--;
-            if (len_data == 0) break;
-        }
+    for (news_article of suggested_content_section.querySelectorAll('.side_article')) {
+        data = news_data[news_data.length-1];
+        
+        article_content = news_article.querySelector('.side-article .side-text-content');
+        article_content.querySelector('.title').innerHTML = `${data.Title}`;
+        article_content.querySelector('.extract').innerHTML = `${data.short_description}`;            
+        article_content.querySelector('.read-more').href = `/templates/news/index.html?id=${data.documentId}`;            
 
-        for (news_article of suggested_content_section.querySelectorAll('.side_article')) {
-            data = news_data[news_data.length-1];
-            
-            article_content = news_article.querySelector('.side-article .side-text-content');
-            article_content.querySelector('.title').innerHTML = `${data.Title}`;
-            article_content.querySelector('.extract').innerHTML = `${data.short_description}`;            
-
-            image_container = news_article.querySelector('.image-container img').src = `${data.image_url}`;
-            
-            news_data.pop();
-            len_data--;
-            if (len_data == 0) break;
-        }
+        image_container = news_article.querySelector('.image-container img').src = `${data.image_url}`;
+        
+        news_data.pop();
+        len_data--;
+        if (len_data == 0) break;
     }
     
 })
 .catch(error => console.error('Error loading news:', error));
-
-/*
-// Loop through the news items
-    newsData.forEach((newsItem, index) => {
-        // Create a container for the news article
-        const articleDiv = document.createElement('div');
-        articleDiv.classList.add('article');
-
-        // Add content to the article
-        articleDiv.innerHTML = `
-            <img src="${newsItem.image_url}" alt="${newsItem.Title}" />
-            <h2>${newsItem.Title}</h2>
-            <p>${newsItem.short_description}</p>
-        `;
-
-        // Append to the appropriate section
-        if (index < 3) {
-            // Add to "Top Stories" (first 3 items)
-            topStoriesSection.appendChild(articleDiv);
-        } else {
-            // Add to "Suggested Content" (remaining items)
-            const sideArticleDiv = document.createElement('div');
-            sideArticleDiv.classList.add('side_article');
-            sideArticleDiv.innerHTML = `
-            <img src="${newsItem.image_url}" alt="${newsItem.Title}" />
-            <h3>${newsItem.Title}</h3>
-            `;
-            suggestedContentSection.appendChild(sideArticleDiv);
-        }
-    });
-*/
---
+++ templates/news/index.js
@@ -0,0 +1,75 @@
+// Fetch the JSON data
+fetch('/resources/json/news.json')
+    .then(response => response.json())
+    .then(data => {
+        data = data.data; // Access the "data" array in the JSON
+        const principal_news_html = document.querySelector('.principal_news');
+
+        const news_id = new URLSearchParams(window.location.search).get("id");
+        news_data = null;
+        author_data = null;
+        for (news of data) {
+            if (news.documentId == news_id) {
+                news_data = news;
+                author_data = news.user;
+                break;
+            }
+        }
+        news_content = "";
+        for (content of news_data.Content) {
+            paragraph = content.children[0].text
+            if (paragraph == "") continue;
+            news_content += `<p>${paragraph}</p>`;
+        }
+
+        principal_news_html.querySelector('h1').innerHTML = `${news_data.Title}`;
+        principal_news_html.querySelector('img').src = `${news_data.image_url}`;
+
+        const author_html = document.querySelector('.author-info')
+        author_html.querySelector('h4').innerHTML = `${author_data.name} \ EE.UU.`;
+        author_html.querySelector('p').innerHTML = `Actualizado - ${news_data.updatedAt.substring(0, 10)} ${news_data.updatedAt.substring(11, 19)}`;
+
+        principal_news_html.querySelector('.content').innerHTML = news_content;
+
+        comments = news_data.comments;
+        fetch('/resources/json/comments.json')
+            .then(response => response.json())
+            .then(comments_data => {
+                comments_data = comments_data.data; // Access the "data" array in the JSON
+                for (comment of document.querySelectorAll(".comment")) {
+                    comment_id = comments[comments.length - 1].documentId;
+                    for (comment_data of comments_data) {
+                        if (comment_data.documentId == comment_id) {
+                            comment.querySelector('h3').innerHTML =  `${comment_data.users_permissions_user.username} ${comment_data.publishedAt.substring(0, 10)}`;
+                            comment.querySelector('p').innerHTML = comment_data.Content;
+                        }
+                    }
+
+                    comments.pop();
+                    if (comments.length == 0) break;
+                }
+            }
+            )
+            .catch(error => console.error('Error loading news:', error));
+
+        len_data = data.length;
+        related = document.querySelectorAll('.related');
+        for (news of related) {
+            news_data = data[data.length - 1];
+            if (news_data.documentId == news_id) {
+                data.pop();
+                len_data--;
+                if (len_data == 0) break;
+                news_data = data[data.length - 1];
+            }
+            news.querySelector('h3').innerHTML = `${news_data.Title}`;
+            news.querySelector('img').src = `${news_data.image_url}`;
+            news.querySelector('p').innerHTML = `<a href="/templates/news/index.html?id=${news_data.documentId}">${news_data.short_description}<a>`;
+
+            data.pop();
+            len_data--;
+            if (len_data == 0) break;
+        };
+
+    })
+    .catch(error => console.error('Error loading news:', error));
