d2cc98dc7dee7406c998f4a137a17a9910ba10a2
TheRubils <rubengc2120@gmail.com>
2025-03-23 02:01:16 +0000

    Added "add_news" (JS) and more
    I added "add_news" code in JS and make little changes in the proyect

--- templates/register/index.js
+++ templates/register/index.js
@@ -5,14 +5,34 @@ document.addEventListener("DOMContentLoaded", function () {
     const userName = document.querySelector("input[type='text']");
     const form = document.querySelector("form");
 
-    emailInput.addEventListener("input", function () {
+    emailInput.addEventListener("input", async function () {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const { correoDuplicado } = await emailVerify(emailInput.value, userName.value); // Corregido aquí
+    
         if (!emailRegex.test(emailInput.value)) {
             emailInput.setCustomValidity("Por favor, ingrese un email válido.");
+        } else if (correoDuplicado) {
+            emailInput.setCustomValidity("Este correo ya está registrado.");
         } else {
             emailInput.setCustomValidity("");
         }
+        emailInput.reportValidity();
     });
+
+    async function emailVerify(email,username) {
+        try {
+            const response = await fetch("/resources/json/users.json");
+            const usuarios = await response.json();
+
+            const nombreDuplicado = usuarios.some(user => user.username === username);
+            const correoDuplicado = usuarios.some(user => user.email === email);
+
+            return {nombreDuplicado,correoDuplicado};
+        } catch (error) {
+            console.error("Error al cargar los usuarios:", error);
+            return {nombreDuplicado:false, correoDuplicado:false};
+        }
+    }
 
     passwordInput.addEventListener("input", function () {
         if (passwordInput.value.length < 6) {
@@ -32,15 +52,17 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    userName.addEventListener("input", function () {
+    userName.addEventListener("input", async function () {
         const regex = /^[a-zA-Z_]+$/;
-
+        const { nombreDuplicado } = await emailVerify(emailInput.value, userName.value);
         if (userName.value.length < 4) {
             userName.setCustomValidity("El nombre de usuario debe tener al menos 4 caracteres.");
         } else if (userName.value.includes(" ")) {
             userName.setCustomValidity("El nombre de usuario no puede contener espacios.");
         } else if (!regex.test(userName.value)) {
             userName.setCustomValidity("Solo puede contener letras y guiones bajos (_).");
+        } else if (nombreDuplicado){
+            userName.setCustomValidity("El nombre de usuario ya esta siendo usado");
         } else {
             userName.setCustomValidity("");
         }
